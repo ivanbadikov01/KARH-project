ed42b5b1882d070b50ed8c999abbc259a4b1f74f
Peace <ibadikovtues2020@gmail.com>
2019-04-25 21:09:25 +0300

    hope this works

--- get-pin.js
+++ get-pin.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
     console.log("kurwi sbogom");
     $("#submit").click(function() {
-
-        var pin = $("input[name='pin']").val();
-        console.log(typeof pin);
-        pin = parseInt(pin, 10);
-        console.log(typeof pin);
-        $.get('\\pin?=', function(data) {
-        	console.log(data);	
-        });
+        var url = window.location.href;
+        var pin = $("input[name=pin]").val();
+        window.location.replace(url + "/" + pin);
     });
 });
