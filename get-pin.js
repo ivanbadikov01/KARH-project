a9dd004c0b521f8fe818410353a4f3eec47283a0
Peace <ibadikovtues2020@gmail.com>
2019-04-25 22:11:12 +0300

    ne znam veche

--- get-pin.js
+++ get-pin.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
     console.log("kurwi sbogom");
     $("#submit").click(function() {
-        var url = window.location.href.split("/")[3];
-<<<<<<< HEAD
+        var url = window.location.href.split("/")[2];
  		console.log(url);
-=======
->>>>>>> 9f3be4911e3b174947e8e7935b99841549a85b0f
         var pin = $("input[name=pin]").val();
         console.log(url + pin);
         window.location.replace(url + pin);
