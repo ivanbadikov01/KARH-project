ff4bbef019cedbfae60b50dad9f17165c7ca90c0
Peace <ibadikovtues2020@gmail.com>
2019-04-25 23:12:52 +0300

    oh boy

--- get-pin.js
+++ get-pin.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
     console.log("kurwi sbogom");
     $("#submit").click(function() {
         var url = window.location.href.split("/")[1];
- 		console.log(url);
         var pin = $("input[name=pin]").val();
-        console.log(url + pin);
-        window.location.replace(url + pin);
+        var state = $("input[name=state").val();
+
+        //window.location.replace(url + pin + state);
+        $("body").append(url + ' ' pin + ' ' state);
     });
 });
