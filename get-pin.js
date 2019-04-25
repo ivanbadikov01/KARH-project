cae72671475de0cbefb8d08d08ffa4d79876da15
ivanbadikov01 <43810114+ivanbadikov01@users.noreply.github.com>
2019-04-25 21:23:08 +0300

    Update get-pin.js

--- get-pin.js
+++ get-pin.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     console.log("kurwi sbogom");
     $("#submit").click(function() {
-        var url = window.location.href;
+        var url = window.location.href.split("/").toArray()[0];
         var pin = $("input[name=pin]").val();
-        window.location.replace(url + pin);
+        window.location.replace(url + "/" + pin);
     });
 });
