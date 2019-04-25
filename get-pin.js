ceedc02c60c033ab63938a151fd54121b7f66c75
Peace <ibadikovtues2020@gmail.com>
2019-04-25 12:15:09 +0300

    meow

--- get-pin.js
+++ get-pin.js
@@ -5,5 +5,6 @@ $(document).ready(function() {
         var pin = $("input[name='pin']").val();
         console.log(typeof pin);
         pin = parseInt(pin, 10);
+        console.log(typeof pin);
     })
 });
