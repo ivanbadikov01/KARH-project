543ee6e68fbe26cf2018daf21d41877cc98779c6
ivanbadikov01 <43810114+ivanbadikov01@users.noreply.github.com>
2019-04-25 11:54:08 +0300

    Add files via upload

--- get-pin.js
+++ get-pin.js
@@ -1,9 +1,8 @@
-$(document).ready(function () {
-	console.log("kurwi sbogom");
-	$("#submit").click(function () {
-		preventFormSubmit();
+$(document).ready(function() {
+    console.log("kurwi sbogom");
+    $("#submit").click(function() {
 
-		var pin = $("input[name='pin']").val();
-		console.log(typeof pin);
-	});
+        var pin = $("input[name='pin']").val();
+        console.log(typeof pin);
+    })
 });
